a86533988ca73c40c224ad88a4e2fa373294a90e
Nolan <nolan.becker@ymail.com>
2017-01-24 13:43:01 -0600

    Fixed issue with scraping unwanted ads. Successfuly scrapes recipe name, description, rating, and author

--- AllRecipesShell.py
+++ AllRecipesShell.py
@@ -8,12 +8,22 @@ soup = BeautifulSoup(pageAllRecipes.content, 'lxml')
 
 for grid in soup.find_all(id="grid"):
     for position in grid.find_all("article", class_=re.compile("grid-col--fixed-tiles$")):
-        name = position.find("ar-save-item")
-        description = position.find(class_="rec-card__description")
-        rating = position.find("div", class_="rating-stars")
+        if position.find("ar-save-item") is not None:
+            nameTag = position.find("ar-save-item")
+            name = nameTag['data-name']
+            description = position.find(class_="rec-card__description").string
+            if description is None:
+                description = "No description found."
+            ratingTag = position.find("div", class_="rating-stars")
+            if ratingTag is None:
+                rating = "No rating found."
+            else:
+                rating = ratingTag['data-ratingstars']
+            authorTag = position.find('h4')
+            author = authorTag.contents[1].strip()
 
-        #print(position.prettify())
-        print(name)
-        print(description)
-        print(rating)
-        print("")
+            print(name)
+            print(description)
+            print("Rating:", rating)
+            print("Recipe by:", author)
+            print("")
