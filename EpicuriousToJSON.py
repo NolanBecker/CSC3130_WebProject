aba81c133c24876a40e3167d721da5c10de00450
Nolan <nolan.becker@ymail.com>
2017-02-07 13:02:19 -0600

    I spelled the file name wrong :/

--- EpicuriousToJSON.py
+++ EpicuriousToJSON.py
@@ -19,17 +19,19 @@ for section in soup.find_all('section', role='main'):
         for article in div.find_all('article', class_='recipe-content-card'):
             data = article.find('header', class_='summary')
             name = data.find(itemprop='name').a.string
-            description = data.p.string
-            rating = data.find('dl', class_='recipes-ratings-summary').find('span', class_='reviews-count-container')\
+            if data.p is not None:
+                description = data.p.string
+            else:
+                description = "No Description Found"
+            rating = data.find('dl', class_='recipes-ratings-summary').find('span', class_='reviews-count-container') \
                 .find('dd', class_='rating').find(itemprop='ratingValue').string
-            reviews= data.find('dl', class_='recipes-ratings-summary').find('span', class_='reviews-count-container')\
+            reviews= data.find('dl', class_='recipes-ratings-summary').find('span', class_='reviews-count-container') \
                 .find('dd', class_='reviews-count').string
 
             response.append(OrderedDict([('Name', name),
                                          ('Description', description),
                                          ('Rating', rating+'/4'),
                                          ('Reviews', reviews)]))
-            print(name)
 
 path = '/home/nolan/Programming/CSC3130_WebProject/JSON/'
 if not os.path.exists(path):
