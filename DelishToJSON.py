9ef76365a81833a352331e6e327f6088234192fb
Nolan <nolan.becker@ymail.com>
2017-02-07 13:42:28 -0600

    Created working DelishToJSON.py

---
+++ DelishToJSON.py
@@ -0,0 +1,51 @@
+import requests
+import datetime
+import json
+import os
+import re
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+
+today = str(datetime.datetime.now().date())
+
+response = []
+
+urlDelish = 'http://www.delish.com/recipes/'
+pageDelish = requests.get(urlDelish)
+soup = BeautifulSoup(pageDelish.content, 'lxml')
+
+name = ''
+description = ''
+time = ''
+difficulty = ''
+
+for div in soup.find_all('div', class_='landing-feed--special-content'):
+    for title in div.find_all('a', class_='landing-feed--special-title'):
+        name = title.string
+    for abstract in div.find_all('div', class_='landing-feed--story-abstract'):
+        if abstract is not None:
+            description = abstract.string[1:]
+        else:
+            description = "No description found"
+    for meta in div.find_all('div', class_='landing-feed--recipe-meta'):
+        for metaTime in meta.find_all('div', class_='recipe-info-total-time'):
+            if metaTime is not None:
+                time = metaTime.contents[2]
+        for metaDiff in div.find_all('div', class_='recipe-info-difficulty'):
+            if metaDiff is not None:
+                difficulty = metaDiff.contents[2]
+
+    response.append(OrderedDict([('Name', name),
+                                 ('Description', description),
+                                 ('Time', time),
+                                 ('Difficulty', difficulty)]))
+
+path = '/home/nolan/Programming/CSC3130_WebProject/JSON/'
+if not os.path.exists(path):
+    os.makedirs(path)
+postingsFile = path + today + '.Delish.json'
+
+with open(postingsFile, 'w') as outfile:
+    json.dump(response, outfile, sort_keys=False, indent=2)
+
+outfile.close()
--- schedule.sh
+++ schedule.sh
@@ -2,3 +2,4 @@
 
 /home/nolan/anaconda3/bin/python /home/nolan/Programming/CSC3130_WebProject/AllRecipesToJSON.py
 /home/nolan/anaconda3/bin/python /home/nolan/Programming/CSC3130_WebProject/EpicuriousToJSON.py
+/home/nolan/anaconda3/bin/python /home/nolan/Programming/CSC3130_WebProject/DelishToJSON.py
