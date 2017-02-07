1e1a1a23e36d67ae2ebeede3a7c4f26c26bf972f
Nolan <nolan.becker@ymail.com>
2017-02-07 12:55:53 -0600

    Added the schedule bash file for use with cron

---
+++ schedule.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/nolan/anaconda3/bin/python /home/nolan/Programming/CSC3130_WebProject/AllRecipesToJSON.py
+/home/nolan/anaconda3/bin/python /home/nolan/Programming/CSC3130_WebProject/EpicuriousToJSON.py
